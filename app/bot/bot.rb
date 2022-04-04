f9e5cbdef9da3949734d0885ac93d87ff308fc82
juliangil2424 <juliangil2424@yahoo.fr>
2022-04-04 19:01:21 -0400

    subscribed param

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,7 +2,8 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
-Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+SUBSCRIBED_PARAMS = [ 'messages', 'message_echoes', 'message_deliveries', 'messaging_optins', 'leadgen' ]
+Facebook::Messenger::Subscriptions.subscribe(access_token: @page_token, subscribed_fields: SUBSCRIBED_PARAMS, page_id: @page_id)
 
 Facebook::Messenger::Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
