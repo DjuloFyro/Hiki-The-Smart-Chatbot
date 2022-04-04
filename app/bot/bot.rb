859e22b2b3129e1e0ac56aaf5e13dcf6b701f766
juliangil2424 <juliangil2424@yahoo.fr>
2022-04-04 08:45:59 -0400

    add: new file

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,7 +2,6 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: access_token)
 
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
